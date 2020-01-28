d6bb2d2161fc0b121b200b61e123fc6a3cd53507
Yuki Okuda <ikura.lab@gmail.com>
2020-01-28 13:52:09 +0900

    feat: 最小限のパターンでfreezeに成功

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,16 +1,10 @@
 import sys
 from fbs_runtime.application_context.PySide2 import ApplicationContext
-# from fbs_runtime.application_context import ApplicationContext
-from PySide2.QtWidgets import QApplication, QDialog
-from forms.debugger import Debugger
-from module_a import Button
-from hoge.module_b import Dialog
+from PySide2.QtWidgets import QDialog
 
 
 if __name__ == "__main__":
     appctxt = ApplicationContext()
-    # widget = Button()
-    # widget = Dialog()
-    widget = Debugger()
+    widget = QDialog()
     widget.show()
     sys.exit(appctxt.app.exec_())
