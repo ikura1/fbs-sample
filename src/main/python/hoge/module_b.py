79902c39a4402dd6ec2016513f90ad27aaaa9f66
Yuki Okuda <ikura.lab@gmail.com>
2020-01-28 13:50:39 +0900

    refactor: 検証不十分のため削除

--- src/main/python/hoge/module_b.py
+++
@@ -1,4 +0,0 @@
-from PySide2.QtWidgets import QPushButton, QDialog
-
-class Dialog(QDialog):
-    pass
--- src/main/python/module_a.py
+++
@@ -1,4 +0,0 @@
-from PySide2.QtWidgets import QPushButton
-
-class Button(QPushButton):
-    pass
